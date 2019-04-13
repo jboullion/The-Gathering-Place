fc80f53227633fd7e49ef75d886f5bcf1bdd46e3
James Boullion <jboullion83@gmail.com>
2019-04-13 01:13:20 -0500

    Creating a Separate "Vanilla" app version to display the board separate from WordPress / Chat

--- vanilla/gamearea.php
+++ vanilla/gamearea.php
@@ -1,5 +1,96 @@
 <div id="party-gamearea" class="hand-tool">
-	<?php get_template_part('templates/party/tools', 'dm'); ?>
+    <div id="dm-tools" class="user-game-actions">
+        <div id="tool-select" class="dm-tool">
+            <div id="tool-holder" class="dm-holder">
+                <div id="current-tile" class="tile" style="background-color: #526F35"></div>
+                <div class="tool-spacer"></div>
+                <div id="hand-tool" class="tool active" title="Normal"><i class="fas fa-hand-paper"></i></div>
+                <div id="paint-tool" class="tool" title="Paint"><i class="fas fa-fw fa-paint-roller"></i></div>
+                <div id="fill-tool" class="tool" title="Fill"><i class="fas fa-fw fa-fill-drip"></i></div>
+                <div id="erase-tool" class="tool" title="Erase"><i class="fas fa-fw fa-eraser"></i></div>
+                <div id="highlight-tool" class="tool" title="Highlight"><i class="fas fa-fw fa-highlighter"></i></div>
+                <div id="sample-tool" class="tool" title="Sample"><i class="fas fa-eye-dropper"></i></div>
+                <div class="tool-spacer"></div>
+                <div id="color-tool" class="visual-tool" title="Color"><input id="tile-color" type="color" value="#526F35" class="fas fa-palette" /></div>
+                <div id="texture-tool" class="visual-tool" title="Texture" data-toggle="modal" data-target="#texture-modal"><i class="fas fa-images"></i></div>
+                
+                <!--
+					<div class="tool-spacer"></div>
+                <div id="save-tool" class="visual-tool" title="Save" data-toggle="modal" data-target="#save-modal"><i class="fas fa-save"></i></div>
+                    
+                    <div id="load-tool" class="visual-tool" title="Open"><i class="fas fa-folder-open"></i></div>
+                    <div id="new-tool" class="visual-tool" title="New"><i class="fas fa-file-plus"></i></div>
+                    <div id="copy-tool" class="visual-tool" title="Duplicate"><i class="fas fa-files-medical"></i></div>
+                    <div id="import-tool" class="visual-tool" title="Import"><i class="fas fa-file-import"></i></div>
+                    <div id="clear-tool" class="visual-tool" title="Clear"><i class="fas fa-undo-alt"></i></div>
+                    <div id="new-layer-tool" class="visual-tool" title="New Layer"><i class="fas fa-layer-plus"></i></div>
+                -->
+
+                
+            </div>
+        </div>
+
+    </div>
+
+    <div id="texture-modal" class="modal" tabindex="-1" role="dialog">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-body">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <i class="fas fa-window-close" aria-hidden="true"></i>
+                    </button>
+                    
+                    <h5 class="modal-title">Select Texture</h5>
+                    <div id="texture-holder" data-dismiss="modal" class="dm-holder"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div id="save-modal" class="modal" tabindex="-1" role="dialog">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <form id="save-form">
+                    <div class="modal-body">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <i class="fas fa-window-close" aria-hidden="true"></i>
+                        </button>
+                        
+                        <h5 class="modal-title">Board Name:</h5>
+                        <input id="board-name" name="board_name" value="" maxlength="50" required/>
+
+                        <label for="auto-save"><input type="radio" id="auto-save" name="autosave" value="1" /> Autosave this board while you play?</label>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="submit" class="btn btn-primary">Save</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <div id="load-modal" class="modal" tabindex="-1" role="dialog">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-body">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <i class="fas fa-window-close" aria-hidden="true"></i>
+                    </button>
+                    
+                    <h5 class="modal-title">Your Boards:</h5>
+                    <div id="board-list">
+                        <ul>
+                            <li>Board 1</h1>
+                        </ul>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="submit" class="btn btn-primary">Save</button>
+                </div>
+            </div>
+        </div>
+    </div>
 
 	<div id="gamearea-map-wrapper">
 		<div id="gamearea-map" oncontextmenu="return false;"></div>
@@ -114,8 +205,8 @@ window.onload = function(){
 	GAME.board = {
 		id: 0,
 		name: 'New Board',
-		height: 20,
-		width: 20,
+		height: 30,
+		width: 30,
 		tiles: [],
 		dragging: false,
 		painting: false,
@@ -316,7 +407,7 @@ window.onload = function(){
 
 		var boardSize = (GAME.board.width * defaultTile.tileSize) + 200; //padding
 
-		$gamearea.style.maxWidth = (boardSize + 20) +'px'; //extra space for scroll bar
+		//$gamearea.style.maxWidth = (boardSize + 20) +'px'; //extra space for scroll bar
 		$map.style.height = boardSize +'px';
 		$map.style.width = boardSize +'px';
 		
@@ -797,7 +888,7 @@ window.onload = function(){
 
 		return false;
 	};
-
+/*
 	//Save our board for later retrival
 	function saveBoard(autosave){
 
@@ -829,7 +920,7 @@ window.onload = function(){
 			}
 		},'json');
 	}
-	
+	*/
 /*
 	//Handle our board submission
 	$saveForm.onsubmit = function(e){
